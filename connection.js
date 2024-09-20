db80b0e06ba8737948a7d70af1cb23ff0aedb983
shivani-rd <shivanichauhan992775@gmail.com>
2024-09-20 14:43:28 +0530

    updation

---
+++ connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+ async function connection(){
+    try{
+        await mongoose.connect('mongodb://localhost:27017/batch-3');
+        console.log("data base connected....")
+    }catch{
+        console.log(err);
+    }
+}
+module.exports = connection;
